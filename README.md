245e859f9238c8ef8932880d66a4c46eaa6b38b8
Dan Faerch <seqcheck.dan@hacker.dk>
2015-09-27 23:49:31 +0200

    stdout and cidr merging

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ip-shuffler
 Randomizing of IP addresses from CIDR ranges for scanning tools like Nmap
+
+## Description
+
+This tool reads a text file, containing networks in CIDR notation (or pure IPs) and outputs a randomized list of all IPs, from all subnets, to either STDOUT or to one or more files.
+
+
+## Usage
+    $ ./ip-shuffler.py input_file  [ out_dir/ [ batch_size ] ]
+
+where:
+
+* input_file:  file containing a list (or mixture) of CIDR notations or ip-adresses (1 per line).
+* out_dir   :  directory where to write all the output files. Will print to STDOUT if omitted.
+* batch_size:  number if ips per output file. (default: 31000)
+
+
+## Examples
+### iput_file example
+
+    172.16.99.1/24
+    10.0.0.10/20
+    127.0.0.1
+    10.0.0.12/24
+    192.168.1.1/25
+
+
+This example highlights 2 points:
+
+1. Mixing if regular IPs with CIDR notation is perfectly valid.
+2. There are 2 CIDR ranges that are actually overlapping on the list. These will
+automagically be merged, thus removing duplicate IPs.
+
+### Usage example
+
+Make a file containing networks. Eg. use the above example listing and save as "example_ip_list"
+
+Make a directory to hold the output files
+
+    $ mkdir outdir
+
+Now run ip-shuffler on the example list, writing files to "outdir" with 1000 ip's per file.
+
+    $ ./ip-shuffler.py example_ip_list outdir 1000
+    Reading example_ip_list
+     - writing outdir/iplist-00000000.out
+     - writing outdir/iplist-00000001.out
+     - writing outdir/iplist-00000002.out
+     - writing outdir/iplist-00000003.out
+     - writing outdir/iplist-00000004.out
+
+## Copyright and license
+
+Code released under GPLv2 (see included LICENSE file). Copyright 2015 Dan Faerch - NullQ.com
+
+
+
--- ip-shuffler.py
+++ ip-shuffler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from netaddr import *
 import random, sys, os
+import pprint;
 
 def usage(err):
   print "ERROR: %s\nUsage:\n $ %s input_file  out_dir/ [ batch_size ]" % (err,sys.argv[0]);
@@ -10,18 +11,24 @@ def usage(err):
   print "  batch_size:  number if ips per output file"
   exit(1)
 
-
-if (len(sys.argv)<3):
+stdout = 0;
+if (len(sys.argv)<2):
   usage("Not enough arguments")
 
+if (len(sys.argv)>2):
+  out_dir = sys.argv[2]
+else:
+  stdout  = 1;
+
+ip_list_file = sys.argv[1]
+
 # Default batch size. Can be overriden on cmdline
-batch_size = 31234;
+batch_size = 31000;
 
 # out-file counter
 step = 0;
 
-ip_list_file = sys.argv[1]
-out_dir      = sys.argv[2]
+
 
 if (len(sys.argv)>3):
   batch_size = int(sys.argv[3])
@@ -29,24 +36,29 @@ if (len(sys.argv)>3):
 if not os.path.isfile(ip_list_file):
   usage(ip_list_file+" is not a file")
 
-if not os.path.isdir(out_dir):
-  usage(out_dir+" is not a dir")
+if not stdout and not os.path.isdir(out_dir):
+    usage(out_dir+" is not a dir")
 
 try:
   f = open(ip_list_file, 'r')
 except IOError:
   print "File open failed!"
-  exit();    
-    
-ip_list = []
+  exit();
+
+cidr_list = []
 
 print "Reading %s" % (ip_list_file);
 
 for line in f:
-  if "/" in line:
-    ip_list.extend(list(IPNetwork(line)))
-  else:
-    ip_list.append(IPAddress(line))
+  if "/" not in line:
+    line.append("/32")
+  cidr_list.append(IPNetwork(line))
+
+# Expand networks into individual IPs
+ip_list = []
+# cidr_merge to remove overlapping subnets.
+for line in cidr_merge(cidr_list):
+  ip_list.extend(list(IPNetwork(line)))
 
 random.shuffle(ip_list)
 ip_list_max = len(ip_list)
@@ -56,18 +68,25 @@ done=0
 # iterate until there are no more IPs
 while not done:
 
-  filename = "%s/iplist-%08d.out" % (out_dir,step);
-  print " - writing %s" % (filename);
-  o = open(filename,'w')
+  if not stdout:
+    filename = "%s/iplist-%08d.out" % (out_dir,step)
+    print " - writing %s" % (filename)
+    o = open(filename,'w')
 
   for x in range( step*batch_size, ((step+1) * batch_size) ):
+    #Are we done?
     if (x>ip_list_max-1):
       done=1
       continue
 
-    o.write(str(ip_list[x])+"\n")
+    if stdout:
+      print str(ip_list[x]);
+    else:
+      o.write(str(ip_list[x])+"\n")
 
-  o.close()
+  if not stdout:
+    o.close()
+
   step=step+1
-    
+
 
